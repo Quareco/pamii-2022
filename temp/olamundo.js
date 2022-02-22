1e2e325f98bf54b5991542acabd20bed60def3b9
quareco <alex_clarinete123@hotmail.com>
2022-02-21 22:44:06 -0300

    Supimpa

---
+++ temp/olamundo.js
@@ -0,0 +1,27 @@
+console.log('Olá mundo dos biscoitinhos');
+
+//let local
+//var escopo maior
+//const uma constante onipresente
+
+//NaN = Not a Number
+
+var a = 2;
+var b = 3;
+let nome = "Alex"
+
+
+console.log("Biscoitinhos totais: " , a);
+
+console.log("Chegaram mais 3 biscoitinhos no mundo\nBiscoitinhos totais: ", a+b);
+
+console.log("Tinhamos 3 biscoitinhos no mundo e 2 foram comidos\nBiscoitinhos totais: ", b-a);
+
+console.log("Fizemos uma grandiosa mágica e nossos 2 biscoitinhos foram multiplicados por 3\nBiscoitinhos totais: ", a*b);
+
+console.log("Por algum motivo decidimos dividr os nossos 2 biscoitinhos por 3 (nenhum biscoito foi ferido no processo\nBiscoitinhos totais: ", a/b);
+
+console.log("Todos os biscoitinhos sumiram");
+
+console.log("Quem comeu todos?\nFoi o ", nome);
+
